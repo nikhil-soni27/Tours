7e4a55ecfee63a07030b0b42ba6835fba2d1bf84
nikhil <nikhilsoni1030@gmail.com>
2021-09-14 10:35:01 +0530

    committer

---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>RUKO ZARA SABAR KRO</h1>
+    </div>
+  );
+};
+
+export default Loading;
